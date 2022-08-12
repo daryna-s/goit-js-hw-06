c67342545992c8e0524eeb1e6fa2d1796c1b6209
daryna-s <lypenn@gmail.com>
2022-08-12 14:16:31 +0300

    done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+function getListContent() { 
+  let result = []; 
+  
+for (let i = 0; i < ingredients.length; i += 1) { 
+let li = document.createElement('li'); 
+  li.append(i);
+
+  li.textContent = `${ingredients[i]}`;
+  li.classList.add(`item`);
+
+  result.push(li);
+} 
+  return result;
+  
+} 
+
+const ul = document.querySelector('#ingredients');
+
+ul.append(...getListContent());
+
+
+
+
