711e232094ee69344bd4009a8107872c4fd288f1
daryna-s <lypenn@gmail.com>
2022-08-19 12:00:23 +0300

    done task-07

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,6 @@ const input = document.querySelector('#name-input');
 
 const label = document.querySelector('#name-output');
 
-input.addEventListener('input', (event) => {
+input.addEventListener('input', event => {
   label.textContent = event.currentTarget.value ? `${event.currentTarget.value}` : `Anonymous`;
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
 const textInput = document.querySelector('#validation-input');
 
 textInput.addEventListener('blur', (event) => {
-  
-  event.currentTarget.value.length === Number(event.currentTarget.dataset.length)
+    
+event.currentTarget.value.length === Number(event.currentTarget.dataset.length)
     ? event.currentTarget.classList.add(`valid`)
     : event.currentTarget.classList.add(`invalid`);
 });
