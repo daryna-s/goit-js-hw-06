9d50668eeb1373cf33cd086aee37b6814ec0f0e8
daryna-s <lypenn@gmail.com>
2022-08-19 22:06:05 +0300

    update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,16 @@
 const textInput = document.querySelector('#validation-input');
 
-textInput.addEventListener('blur', (event) => {
+textInput.addEventListener('blur', validationInput);
+
+function validationInput(a) {
+ const validTargetLength = a.currentTarget.value.length;
+    const inputLength = Number(a.currentTarget.dataset.length);
     
-event.currentTarget.value.length === Number(event.currentTarget.dataset.length)
-    ? event.currentTarget.classList.add(`valid`)
-    : event.currentTarget.classList.add(`invalid`);
-});
+    if (validTargetLength === inputLength) {
+        a.currentTarget.classList.remove(`invalid`);
+        a.currentTarget.classList.add(`valid`);
+    } else {
+        a.currentTarget.classList.remove(`valid`);
+        a.currentTarget.classList.add(`invalid`);
+    }
+}
