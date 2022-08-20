1c9ed4bcfaa7ea78e42d33dea6cd77d71812778e
daryna-s <lypenn@gmail.com>
2022-08-20 19:12:06 +0300

    done task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,73 +1,38 @@
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-// const ref = {
-// controls: document.querySelector(`#controls`);
-// input: document.querySelector(`input`);
-// createBtn: document.querySelector(`[data - create]`);
-// destroyBtn: document.querySelector(`[data-destroy]`);
-// }
-// const createBoxes = document.querySelector(`#boxes`);
-
-// const total = 0;
-// function createBoxes(amount) {
-//   for (let i = 1; i < 100; i += 1) {
-//     total += i;
-//   }
-// }
+const ref = {
+controls: document.querySelector(`#controls`),
+input: document.querySelector(`input`),
+createBtn: document.querySelector(`[data-create]`),
+destroyBtn: document.querySelector(`[data-destroy]`),
+}
+const boxEl = document.querySelector(`#boxes`);
 
 
+ref.createBtn.addEventListener(`click`, createBoxes);
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function createBoxes(amount) {
+    const divs = [];
+    amount = ref.input.value;
 
-// const refs = {
-//   inputEl: document.querySelector("#controls > input"),
-//   createBtnEl: document.querySelector("[data-create]"),
-//   destroyBtnEl: document.querySelector("[data-destroy]"),
-//   boxesCountainerEl: document.querySelector("#boxes"),
-// };
+    for (let i = 0; i < amount; i += 1){
+        let div = document.createElement('div');
+        div.style.backgroundColor = getRandomHexColor();
+        div.style.width = `${30 + i * 10}px`;
+        div.style.height = `${30 + i * 10}px`;
 
-// let baseSizes = 20;
-// let addValue = 10;
+        divs.push(div);
+    };
 
-// refs.inputEl.addEventListener("input", onInputClick);
+    boxEl.append(...divs);
+};
 
-// //! Получаем значение amount
-// function onInputClick(event) {
-//   let amount = event.currentTarget.value;
-//   console.log("Количество дивов -", amount);
+ref.destroyBtn.addEventListener(`click`, destroyBoxes);
 
-//   const elements = [];
-
-//   refs.createBtnEl.addEventListener("click", createBoxes);
-//   refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
-
-//   //! Цикл создания div
-//   function createBoxes() {
-//     for (let i = 1; i <= amount; i += 1) {
-//       console.log("amount", amount);
-
-//       const divEl = document.createElement("div");
-//       divEl.style.backgroundColor = getRandomHexColor();
-//       divEl.style.width = baseSizes + addValue + `px`;
-//       divEl.style.height = baseSizes + addValue + `px`;
-//       addValue += 10;
-//       console.log("width", divEl.style.width);
-//       console.log("height", divEl.style.height);
-//       elements.push(divEl);
-//     }
-//     refs.boxesCountainerEl.append(...elements);
-//   }
-
-//   //! Очищаем div#boxes
-//   function onDestroyBtnElClick() {
-//     refs.boxesCountainerEl.innerHTML = "";
-//     refs.inputEl.value = "";
-
-//     console.log("div#boxes очищен!", amount);
-//   }
-// } 
+function destroyBoxes() {
+    boxEl.innerHTML = "";
+    ref.input.value = '';
+};
